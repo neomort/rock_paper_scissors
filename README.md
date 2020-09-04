e3c2dfbed59efefac7e29d3108356355818a9023
neomort <kyle@kyleadams.com>
2020-09-04 17:10:17 -0600

    styled a bit

--- README.md
+++ README.md
@@ -16,3 +16,17 @@ Player beats computer or vise versa (winner)
 
 
 reset function
+
+
+//player choice clicks on rock, paper, or scissors tag in html
+
+
+
+// ok so now if you chose rock, paper, or scissors, what happens?
+// right now it prints it out to terminal, but we could also print it in html maybe?
+//also we need the functions chooseRock etc.. to trigger win or loss based on computer's answer
+// function decideGame(){
+// const { playerChoice, computerChoice } = state;
+// state.message = `<div> Player chose: ${playerChoice} <hr /> Computer chose: ${computerChoice}</div>`
+// // checkGame(playerChoice, computerChoice )
+// }
--- index.js
+++ index.js
@@ -14,18 +14,7 @@ function computerChooses(){
   state.computerChoice = state.choices[Math.floor(Math.random() * 3)]
   console.log(state.computerChoice);
 }
-//player choice clicks on rock, paper, or scissors tag in html
 
-
-
-// ok so now if you chose rock, paper, or scissors, what happens?
-// right now it prints it out to terminal, but we could also print it in html maybe?
-//also we need the functions chooseRock etc.. to trigger win or loss based on computer's answer
-// function decideGame(){
-// const { playerChoice, computerChoice } = state;
-// state.message = `<div> Player chose: ${playerChoice} <hr /> Computer chose: ${computerChoice}</div>`
-// // checkGame(playerChoice, computerChoice )
-// }
 
 function checkGame(playerChoice, computerChoice ){
   computerChooses();
@@ -81,7 +70,7 @@ function render(){
   <div>
     <h1>Rock Paper Scissors</h1>
 
-    <div onclick='computerChooses()'>
+    <div>
     ${state.message}
     </div>
     
@@ -102,9 +91,9 @@ function render(){
     </div>
     </div>
     <br />
-    <div>
+    <h3>
     ${state.resultMessage}
-    </div>
+    </h3>
 
 
   </div>
@@ -114,3 +103,4 @@ function render(){
 }
 
 render();
+
